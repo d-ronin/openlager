cc575fda5a598300a8bf995cac9797068e0646af
Michael Lyle <mlyle@lyle.org>
2016-05-01 22:46:15 -0700

    Introduce support for DMA on write. (#44)
    Fixes #30.

--- src/openlager.c
+++ src/openlager.c
@@ -404,7 +404,8 @@ int main() {
 			RCC_AHB1Periph_GPIOB |
 			RCC_AHB1Periph_GPIOC |
 			RCC_AHB1Periph_GPIOD |
-			RCC_AHB1Periph_GPIOE,
+			RCC_AHB1Periph_GPIOE |
+			RCC_AHB1Periph_DMA2,
 			ENABLE);
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 |
